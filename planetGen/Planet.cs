1ef75925f417c6e478b5b263e2bfb86e124c7cd2
Jon <47305276+Captain-Seli@users.noreply.github.com>
2023-01-23 06:53:09 -0500

    Commit 3: Planet Class started to house data.

---
+++ planetGen/Planet.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace planetGen
+{
+    /// <summary>
+    /// Planet Class stores all the data from the enums
+    /// </summary>
+    public  class Planet
+    {
+        
+        public string Atmosphere { get; set; }
+        public string Climate { get; set; }
+        public string Civilization { get; set; }
+        public string Gravity { get; set; }
+        public string Orbitals { get; set; }
+        public string PrimeBiome { get; set; }
+        public string SecBiome { get; set; }
+        public string PlanetaryFeatures { get; set; }
+        public string Quirks { get; set; }
+        public string Size { get; set; }
+        public string Type { get; set; }
+        public string WeatherFeature { get; set; }
+        public string WeatherPattern { get; set; }
+
+
+    }
+}
